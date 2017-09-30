4554a0502519d104d568bc012f1c8681dc186e1c
Zeiungsleser <ju.lg@gmx.de>
2017-09-30 16:01:14 +0200

    Neues Teil ?

---
+++ entities.js
@@ -0,0 +1,65 @@
+function Manager(){
+  this.solids = [];
+};
+
+Manager.prototype.add_solid = function(solid){
+  this.solids.push(solid);
+};
+
+Manager.prototype.render = function () {
+  for(var i=0;i<this.solids.length;i++){
+    this.solids[i].render();
+  }
+};
+
+Manager.prototype.update = function (t) {
+  if(split_time){
+    for(var j=0; j<=t;j+=t/split_factor){
+      for(var i=0;i<this.solids.length;i++){
+      solid = this.solids[i]
+      solid.update_velocity(t/split_factor, this.solids)
+      this.solids[i].move(t/split_factor);
+      }
+    }
+  }
+  else{
+    for(var i=0;i<this.solids.length;i++){
+      this.solids[i].update_velocity(t, this.solids);
+      this.solids[i].move(t);
+    }
+  }
+};
+
+function Solid(init_posx, init_posy, init_velx, init_vely, mass, bound=false, radius=50){
+  this.position = new p5.Vector(init_posx, init_posy);
+  this.velocity = new p5.Vector(init_velx, init_vely);
+  this.mass = mass;
+  this.bound = bound;
+  this.radius = radius;
+}
+
+Solid.prototype.render = function () {
+  push();
+  translate(this.position.x/pow(10,9), this.position.y/pow(10,9));
+  stroke('white');
+  noFill();
+  ellipse(0,0,this.radius);
+  pop();
+}
+
+Solid.prototype.move = function (t) {
+  this.position.add(new p5.Vector(this.velocity.x*t,this.velocity.y*t))
+}
+
+Solid.prototype.update_velocity =function(t, objects) {
+  if(!this.bound){
+    acceleration = new p5.Vector(0,0);
+    for(var i=0;i<objects.length;i++){
+      r = new p5.Vector(objects[i].position.x-this.position.x, objects[i].position.y-this.position.y);
+      absolute = objects[i].mass/r.magSq();
+      acceleration.add(r.normalize().mult(absolute));
+    }
+    acceleration.mult(G);
+    this.velocity.add(acceleration.mult(t));
+  }
+}
